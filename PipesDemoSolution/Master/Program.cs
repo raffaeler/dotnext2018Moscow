12535653267f6af96b2f29b70479c81fafd27355
raffaeler <raffaeler@vevy.com>
2018-11-19 16:33:16 +0100

    First commit

---
+++ PipesDemoSolution/Master/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullpath = Path.GetFullPath(
+                    Path.Combine(
+                        Path.GetDirectoryName(
+                            Assembly.GetExecutingAssembly().Location),
+                        @"..\..\nasa.jpg"));
+
+            using (var fs = File.OpenRead(fullpath))
+            {
+                using (var stdout = Console.OpenStandardOutput())
+                {
+                    fs.CopyTo(stdout);
+                }
+            }
+        }
+    }
+}
